263e37cf9228a0557a6366700c569bec4085969c
BaiXiaoFei <43197105+Laobai-F@users.noreply.github.com>
2020-11-03 22:37:11 +0800

    Update Test1.java

--- src/servlet/Test1.java
+++ src/servlet/Test1.java
@@ -2,7 +2,7 @@ package servlet;
 
 public class Test1 {
     public static void main(String[] args) {
-        Download download = new Download();
+        Download download01 = new Download();
         Download download12 = new Download();
     }
 }

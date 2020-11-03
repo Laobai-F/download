cab592196645c47f61f09fd7d039ff4037d78f2a
laobai <laobai@163.com>
2020-11-03 21:23:33 +0800

    提交Test1

---
+++ src/servlet/Test1.java
@@ -0,0 +1,9 @@
+package servlet;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Download download = new Download();
+        float currDownloadplan = download.getCurrDownloadplan();
+
+    }
+}

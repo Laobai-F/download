c3dff4dc952d46170214508948ca3b995353d189
BaiXiaoFei <43197105+Laobai-F@users.noreply.github.com>
2020-11-03 21:24:54 +0800

    Update Test1.java

--- src/servlet/Test1.java
+++ src/servlet/Test1.java
@@ -3,7 +3,6 @@ package servlet;
 public class Test1 {
     public static void main(String[] args) {
         Download download = new Download();
-        float currDownloadplan = download.getCurrDownloadplan();
 
     }
 }
